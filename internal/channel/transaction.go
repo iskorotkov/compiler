73490531a87c7dcf64672b78ca3de1af40bf6345
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2022-03-21 08:16:08 +0500

    feat(channel): add transaction channel for using in BNFs

---
+++ internal/channel/transaction.go
@@ -0,0 +1,91 @@
+package channel
+
+import (
+	"sync"
+)
+
+type TransactionChannel[T any] struct {
+	sourceCh <-chan T
+
+	transaction []T
+	rollback    []T
+	closed      bool
+	m           sync.Mutex
+}
+
+func NewTransactionChannel[T any](ch <-chan T) *TransactionChannel[T] {
+	return &TransactionChannel[T]{
+		sourceCh: ch,
+	}
+}
+
+// Commit commits all reads and starts a new transaction.
+func (c *TransactionChannel[T]) Commit() {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	c.transaction = nil
+}
+
+// Rollback adds values from ongoing transaction to the rollback list and starts a new transaction.
+func (c *TransactionChannel[T]) Rollback() {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	c.rollback = append(c.transaction, c.rollback...)
+	c.transaction = nil
+
+	if len(c.rollback) > 0 {
+		c.closed = false
+	}
+}
+
+// Read reads from the rollback list or from source channel.
+func (c *TransactionChannel[T]) Read() T {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	var (
+		nextElement  T
+		fromRollback bool
+	)
+	if len(c.rollback) > 0 {
+		nextElement, c.rollback = c.rollback[0], c.rollback[1:]
+		fromRollback = true
+	}
+
+	if !fromRollback {
+		var open bool
+		nextElement, open = <-c.sourceCh
+		if !open {
+			c.closed = true
+		}
+	}
+
+	c.transaction = append(c.transaction, nextElement)
+
+	return nextElement
+}
+
+// Open checks whether the channel has rollback values or if the source channel is open.
+func (c *TransactionChannel[T]) Open() bool {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	if c.closed {
+		return false
+	}
+
+	if len(c.rollback) > 0 {
+		return true
+	}
+
+	item, open := <-c.sourceCh
+	if open {
+		c.rollback = append([]T{item}, c.rollback...)
+	} else {
+		c.closed = true
+	}
+
+	return !c.closed
+}
---
+++ internal/channel/transaction_test.go
@@ -0,0 +1,91 @@
+package channel_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/iskorotkov/compiler/internal/channel"
+)
+
+func TestChannelWrapping(t *testing.T) {
+	t.Parallel()
+
+	ch := channel.NewTransactionChannel(channel.FromSlice([]int{1, 2, 3}))
+	assert.True(t, ch.Open())
+
+	assert.Equal(t, 1, ch.Read())
+	assert.True(t, ch.Open())
+
+	assert.Equal(t, 2, ch.Read())
+	assert.True(t, ch.Open())
+
+	assert.Equal(t, 3, ch.Read())
+	assert.False(t, ch.Open())
+}
+
+func TestCommit(t *testing.T) {
+	t.Parallel()
+
+	ch := channel.NewTransactionChannel(channel.FromSlice([]int{1, 2, 3}))
+	assert.Equal(t, 1, ch.Read())
+	assert.Equal(t, 2, ch.Read())
+	assert.Equal(t, 3, ch.Read())
+	assert.False(t, ch.Open())
+
+	ch.Commit()
+	assert.False(t, ch.Open())
+}
+
+func TestRollback(t *testing.T) {
+	t.Parallel()
+
+	ch := channel.NewTransactionChannel(channel.FromSlice([]int{1, 2, 3}))
+	assert.Equal(t, 1, ch.Read())
+	assert.Equal(t, 2, ch.Read())
+	assert.Equal(t, 3, ch.Read())
+	assert.False(t, ch.Open())
+
+	ch.Rollback()
+	assert.True(t, ch.Open())
+
+	assert.Equal(t, 1, ch.Read())
+	assert.Equal(t, 2, ch.Read())
+	assert.Equal(t, 3, ch.Read())
+	assert.False(t, ch.Open())
+}
+
+func TestPartialRollback(t *testing.T) {
+	t.Parallel()
+
+	ch := channel.NewTransactionChannel(channel.FromSlice([]int{1, 2, 3}))
+	assert.Equal(t, 1, ch.Read())
+	assert.Equal(t, 2, ch.Read())
+	assert.True(t, ch.Open())
+
+	ch.Rollback()
+	assert.True(t, ch.Open())
+
+	assert.Equal(t, 1, ch.Read())
+	assert.Equal(t, 2, ch.Read())
+	assert.Equal(t, 3, ch.Read())
+	assert.False(t, ch.Open())
+}
+
+func TestCommitRollback(t *testing.T) {
+	t.Parallel()
+
+	ch := channel.NewTransactionChannel(channel.FromSlice([]int{1, 2, 3}))
+	assert.Equal(t, 1, ch.Read())
+	assert.Equal(t, 2, ch.Read())
+	assert.True(t, ch.Open())
+
+	ch.Commit()
+	assert.True(t, ch.Open())
+
+	ch.Rollback()
+	assert.True(t, ch.Open())
+
+	assert.Equal(t, 3, ch.Read())
+	assert.False(t, ch.Open())
+}
