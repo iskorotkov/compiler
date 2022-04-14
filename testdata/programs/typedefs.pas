9d0327ce0ce8ad8fb3a5e88e1cdf586addbd419f
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2022-04-14 12:12:54 +0500

    test: add tests for typedefs and identifier redeclaration

---
+++ testdata/programs/typedefs.pas
@@ -0,0 +1,12 @@
+program p;
+const PI = 3.14;
+      E = 2.71;
+      R = 16;
+type my_type = real;
+     int = integer;
+var p, square, res: real;
+begin
+  p := 16 + 2 * PI * R - 1;
+  square := PI * R * R;
+  res := 14.5 * p + 123 + square + 12.7128;
+end.
